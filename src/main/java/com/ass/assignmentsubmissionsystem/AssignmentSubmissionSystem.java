f8cfea9c97d12bb609620c4d660b780eac1ca1d5
BFO1715 <110555448+BFO1715@users.noreply.github.com>
2026-03-15 17:43:50 +0000

    Initial Commit

---
+++ src/main/java/com/ass/assignmentsubmissionsystem/AssignmentSubmissionSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ass.assignmentsubmissionsystem;
+
+/**
+ *
+ * @author betha
+ */
+public class AssignmentSubmissionSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
